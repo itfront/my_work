cc2a706a298041c341e7f15e5f3ad4563da89c78
Desenvolvimento <jefferson.coelho23@GMAIL.COM>
2021-10-18 21:12:59 -0300

    Criado as classes Modelo Exercise / workout

---
+++ lib/models/exercise.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Exercise {
+  String id;
+  String name;
+  String description;
+  String imageUrl;
+  String workoutId;
+
+  Exercise(this.id, this.name, this.description, this.workoutId, this.imageUrl);
+}
---
+++ lib/models/workout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Workout {
+  String id;
+  String name;
+  String imageUrl;
+  int weekDay;
+
+  Workout(this.id, this.name, this.imageUrl, this.weekDay);
+}
